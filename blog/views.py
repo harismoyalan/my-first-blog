efd95b5e4d7ea9463fce660b3fb4bf2158218037
Haris <harismoyalan@gmail.com>
2017-07-18 14:29:32 +0530

    solved conflict

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 # Create your views here.
-<<<<<<< HEAD
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-=======
 def post_list(request):
 	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
->>>>>>> fc6d63c5ed32e26826c5adb0f371a483621e73a3
